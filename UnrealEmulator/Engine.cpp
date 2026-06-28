ff4a0eb96ca76f3d628ef2d58d57827ac60890b6
anshkunj <anshkunj.ak@gmail.com>
2026-06-28 12:59:52 +0530

    commit

--- UnrealEmulator/Engine.cpp
+++ UnrealEmulator/Engine.cpp
@@ -1,3 +1,18 @@
 #include "Engine/Engine.h"
 UEngine UEngineMock;
 UEngine* GEngine = &UEngineMock;
+
+// Emulator ko chalane ke liye main entry point
+#include "GameFramework/GameModeBase.h"
+
+// Hamare specific game mode class ko yahan declare kar dete hain takia compiler ko pata chale
+class AMyGameMode : public AGameModeBase {
+public:
+    virtual void BeginPlay();
+};
+
+int main() {
+    AMyGameMode GameModeInstance;
+    GameModeInstance.BeginPlay();
+    return 0;
+}
